8e30cc3c305aed95229ff09aa22b0c0327af29da
jberkowitz89 <joey.berkowitz@vercel.com>
2024-10-04 15:32:07 -0500

    creating triangle

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    print('Starting asteroids!')
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #player object
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
-        # screen.flip()
+            
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        
+
+        #limit the framerate to 60 fps
+        dt = (clock.tick(60) / 1000)
+
 if __name__ == "__main__":
     main()
