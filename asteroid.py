f8b4e6da710f7db282a4a098469ba898704da3c0
jberkowitz89 <joey.berkowitz@vercel.com>
2024-10-09 13:21:57 -0500

    splitting

--- asteroid.py
+++ asteroid.py
@@ -19,13 +19,12 @@ class Asteroid(CircleShape):
 
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
-        else:
-            random_angle = random.uniform(20, 50)
-            v1 = self.velocity.rotate(random_angle)
-            v2 = self.velocity.rotate(-random_angle)
-            radius_1 = (self.radius - ASTEROID_MIN_RADIUS)
-            radius_2 = (self.radius - ASTEROID_MIN_RADIUS)
-            asteroid1 = Asteroid(self.position.x, self.position.y, radius_1)
-            asteroid2 = Asteroid(self.position.x, self.position.y, radius_2)
-            asteroid1.velocity = v1 * 1.2
-            asteroid2.velocity = v2 * 1.2
+        
+        random_angle = random.uniform(20, 50)
+        v1 = self.velocity.rotate(random_angle)
+        v2 = self.velocity.rotate(-random_angle)
+        new_radius = (self.radius - ASTEROID_MIN_RADIUS)
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = v1 * 1.2
+        asteroid2.velocity = v2 * 1.2
